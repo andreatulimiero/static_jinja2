2fa084b3fe975a49c3d89564692bfcbca889ef65
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2017-04-15 22:30:11 +0200

    Added samples

---
+++ templates/app.js
@@ -0,0 +1,5 @@
+let debugButton = document.querySelector('.debug-button');
+if (debugButton.innerText == 'on')
+    debugButton.style.backgroundColor = '#8BC34A';
+else
+    debugButton.style.backgroundColor = '#BF360C';
